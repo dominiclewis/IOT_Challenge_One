a8861a48d7acf8b76e6fc872e9fc24a68130e20d
Dominic Lewis <dominic2.lewis@live.uwe.ac.uk>
2018-01-31 23:06:37 +0000

    BUTTONS WORK YESSS

--- build/bbc-microbit-classic-gcc/yotta_build_info.h
+++ build/bbc-microbit-classic-gcc/yotta_build_info.h
@@ -4,11 +4,11 @@
 #define YOTTA_BUILD_YEAR 2018 // UTC year
 #define YOTTA_BUILD_MONTH 1 // UTC month 1-12
 #define YOTTA_BUILD_DAY 31 // UTC day 1-31
-#define YOTTA_BUILD_HOUR 13 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 30 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 37 // UTC second 0-61
-#define YOTTA_BUILD_UUID c4c55047-43ee-44e3-997d-4d7f196bb720 // unique random UUID for each build
-#define YOTTA_BUILD_VCS_ID 5109bd9b366c07f3f149942b6b9d0d49cbab4e16 // git or mercurial hash
+#define YOTTA_BUILD_HOUR 23 // UTC hour 0-24
+#define YOTTA_BUILD_MINUTE 5 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 18 // UTC second 0-61
+#define YOTTA_BUILD_UUID 999a1dd9-7ab1-4ed2-8adb-787cec313760 // unique random UUID for each build
+#define YOTTA_BUILD_VCS_ID d15457e179236e477b19c457be4a5539c51702a7 // git or mercurial hash
 #define YOTTA_BUILD_VCS_CLEAN 0 // evaluates true if the version control system was clean, otherwise false
-#define YOTTA_BUILD_VCS_DESCRIPTION 5109bd9 // git describe or mercurial equivalent
+#define YOTTA_BUILD_VCS_DESCRIPTION d15457e // git describe or mercurial equivalent
 #endif // ndef __YOTTA_BUILD_INFO_H__
--- source/main.cpp
+++ source/main.cpp
@@ -9,16 +9,18 @@
 #define PLAYERONEID 1
 #define PLAYERTWOID 2
 
-MicroBit uBit;
-
+MicroBit uBit;//Instance of the MicroBit class
+//MicroBitDisplay display; //Create an instance of 5x5 Led Matrix
+MicroBitImage screen(5,5);
 /*
 * Purpose: Will score player information
 */
 class Player
 {
   public:   //Access specifier
-    Player(int score, bool user) : player_score(score), player_is_user(user)
-    //Apparantly the above initialises the player classes
+    Player(int score, bool user) : player_score(score), player_is_user(user) //Constructor
+    {}
+    //Apparantly the above initialises the player classes without assignments
 
     /*
     * Purpose: This method will return the players score
@@ -41,7 +43,7 @@ class Player
     }
     /*
     *Purpose: Adjusts the score by either incrementing or decrementing by 1.
-    *Accepts: An integer of eithe 1 or -1
+    *Accepts: An integer of either 1 or -1
     *Returns: Boolean - True if an adjustment is carried out, false if otherwise
     */
     bool adjust_score(int adjustment)
@@ -61,29 +63,32 @@ class Player
   private: //Private variables
     int player_score;
     bool player_is_user;
-}
+};
 
-void on_button_A(MicroBitEvent e)
+void on_button_a(MicroBitEvent e)
 {
+  screen.setPixelValue(1,1,1);
 
-  uBit.display.print("World");
+  //screen.print('J');
 }
 
 void on_button_b(MicroBitEvent e)
 {
-  uBit.display.print("Button B");
+  //uBit.display.print("Button B");
+  screen.setPixelValue(1,1,0);
 }
 
 int main(){
 
   uBit.init();
-  uBit.display.scroll("Hello");
-
-  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, on_button_A);
-  uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, on_button_B);
+  //uBit.display.scroll("Hello");
+  //uBit.display.print("Button B"); I prefer printing to scrolling for readability
+  //Below is regisering the event handlers
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, on_button_a);
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, on_button_b);
   while(1){
     uBit.sleep(500);
-
+    uBit.display.image.paste(screen); //update the screen
   }
   release_fiber(); //Release the fiber in main to return control to scheduler
 }
