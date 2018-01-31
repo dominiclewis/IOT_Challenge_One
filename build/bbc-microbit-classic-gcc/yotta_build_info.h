5f5cc94993986b134320e1b80dc4c0ccc25853a5
Dominic Lewis <dominic2.lewis@live.uwe.ac.uk>
2018-01-31 13:33:14 +0000

    Example of Event handling

--- build/bbc-microbit-classic-gcc/yotta_build_info.h
+++ build/bbc-microbit-classic-gcc/yotta_build_info.h
@@ -3,12 +3,12 @@
 // yotta build info, #include YOTTA_BUILD_INFO_HEADER to access
 #define YOTTA_BUILD_YEAR 2018 // UTC year
 #define YOTTA_BUILD_MONTH 1 // UTC month 1-12
-#define YOTTA_BUILD_DAY 30 // UTC day 1-31
-#define YOTTA_BUILD_HOUR 22 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 8 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 25 // UTC second 0-61
-#define YOTTA_BUILD_UUID e2cf2448-2bdd-44be-b005-7f9430b9873a // unique random UUID for each build
-#define YOTTA_BUILD_VCS_ID 30541cefa7b4be294c49a4261bb56557a0718633 // git or mercurial hash
+#define YOTTA_BUILD_DAY 31 // UTC day 1-31
+#define YOTTA_BUILD_HOUR 13 // UTC hour 0-24
+#define YOTTA_BUILD_MINUTE 30 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 37 // UTC second 0-61
+#define YOTTA_BUILD_UUID c4c55047-43ee-44e3-997d-4d7f196bb720 // unique random UUID for each build
+#define YOTTA_BUILD_VCS_ID 5109bd9b366c07f3f149942b6b9d0d49cbab4e16 // git or mercurial hash
 #define YOTTA_BUILD_VCS_CLEAN 0 // evaluates true if the version control system was clean, otherwise false
-#define YOTTA_BUILD_VCS_DESCRIPTION 30541ce // git describe or mercurial equivalent
+#define YOTTA_BUILD_VCS_DESCRIPTION 5109bd9 // git describe or mercurial equivalent
 #endif // ndef __YOTTA_BUILD_INFO_H__
--- source/main.cpp
+++ source/main.cpp
@@ -9,9 +9,25 @@
 
 MicroBit uBit;
 
+void onButtonA(MicroBitEvent e){
+
+  uBit.display.print("World");
+}
+
+void onButtonB(MicroBitEvent e){
+  uBit.display.print("Button B");
+}
+
 int main(){
 
   uBit.init();
-  uBit.display.scroll("Test");
+  uBit.display.scroll("Hello");
+
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+  while(1){
+    uBit.sleep(500);
+
+  }
   release_fiber(); //Release the fiber in main to return control to scheduler
 }
